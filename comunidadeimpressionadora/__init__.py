8bb36452b93167c10102ebfa01a7b47f645e4ada
Pedro Bittencourt <132624980+PedroPVB26@users.noreply.github.com>
2023-08-10 11:16:59 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -1,15 +1,15 @@
-# -------------------- CONFIGURAÇÃO DO SITE --------------------
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
-
-# Configura a chave de segurança e o banco de dados
 app.config['SECRET_KEY'] = 'b3e91ee560e9c76912bb02ebc2622bb2'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dados_do_site.db'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dados_do_site.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -17,5 +17,4 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
-# Fica em baixo porque os routes têm que ficar em baixo do app
 from comunidadeimpressionadora import routes
